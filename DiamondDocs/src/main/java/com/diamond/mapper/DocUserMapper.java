015d18e614edd1764f8f37c3b06e520f3209efac
hhidk <295809364@qq.com>
2020-08-10 18:03:13 +0800

    邮件功能补充，bug修复

--- DiamondDocs/src/main/java/com/diamond/mapper/DocUserMapper.java
+++ DiamondDocs/src/main/java/com/diamond/mapper/DocUserMapper.java
@@ -16,7 +16,7 @@ public interface DocUserMapper {
     增加一个新用户，注册需要
     返回结果为插入条数，结果为0时插入失败
      */
-    int addDocUser(@Param("docUser") DocUser docUser);
+    int addDocUser(DocUser docUser);
 
     /*
     根据UserName获取用户所有信息*
@@ -41,34 +41,34 @@ public interface DocUserMapper {
     返回结果为更新行数，结果为0时更新失败
     使用map传参，key分别为userID和userName
      */
-    int updateDocUserName(@Param("map") Map<String, Object> map);
+    int updateDocUserName(Map<String, Object> map);
 
     /*
     修改用户信息
     返回结果为更新行数，结果为0时更新失败
     使用map传参，key分别为userID和userPassword
      */
-    int updateDocUserPassword(@Param("map") Map<String, Object> map);
+    int updateDocUserPassword(Map<String, Object> map);
 
     /*
     修改用户信息
     返回结果为更新行数，结果为0时更新失败
     使用map传参，key分别为userID和mobile
      */
-    int updateDocUserMobile(@Param("map") Map<String, Object> map);
+    int updateDocUserMobile(Map<String, Object> map);
 
     /*
     修改用户信息
     返回结果为更新行数，结果为0时更新失败
     使用map传参，key分别为userID和emailAddress
      */
-    int updateDocUserEmailAddress(@Param("map") Map<String, Object> map);
+    int updateDocUserEmailAddress(Map<String, Object> map);
 
     /*
     修改用户信息
     返回结果为更新行数，结果为0时更新失败
     使用map传参，key分别为userID和imagePath
      */
-    int updateDocUserImagePath(@Param("map") Map<String, Object> map);
+    int updateDocUserImagePath(Map<String, Object> map);
 
 }
