ae7ee4211c064c0dab00e3098e2b6bd17aaa4ca3
Tonylyc <2290549567@qq.com>
2020-08-10 23:25:24 +0800

    工作台页面和Button的修改

---
+++ vuec/src/views/Home/WorkSpace/MyCollection/MyCollection.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="docs">
+    <div class="doc">
+      <img src="@/assets/icon/header/icon-litst.png" class="doc-icon" />
+      <span>Test Document</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Recent",
+};
+</script>
+
+<style scoped>
+.docs {
+  display: flex;
+  margin: 20px;
+}
+
+.doc {
+  align-items: center;
+  display: flex;
+  width: 300px;
+  height: 80px;
+  background-color: #e1e2e4;
+  margin: 20px;
+}
+
+.doc-icon {
+  height: 60px;
+  margin: 10px;
+}
+</style>
