d2bdeff0708053f4ce26c0f2c92c7844ea27ebf4
hhidk <295809364@qq.com>
2020-08-10 16:49:33 +0800

    注册功能

--- DiamondDocs/src/main/java/com/diamond/service/UserInfoService.java
+++ DiamondDocs/src/main/java/com/diamond/service/UserInfoService.java
@@ -2,6 +2,7 @@ package com.diamond.service;
 
 import com.diamond.mapper.DocUserMapper;
 import com.diamond.pojo.DocUser;
+import com.diamond.utils.DiyUUID;
 import com.diamond.utils.Validation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,13 +16,19 @@ public class UserInfoService {
     public int signUp(String name, String emailAddress, String password, String password2) throws Exception{
         if(!Validation.checkEmailFormat(emailAddress))
             return 1;
-        else if(false)
+        else if(docUserMapper.getDocUserByEmailAddress(emailAddress)!=null)
             return 4;
         else if(!password.equals(password2))
             return 2;
         else if(!Validation.checkPasswordFormat(password))
             return 3;
-        else
-            return 5;
+
+        DocUser user = new DocUser();
+        user.setUserID(DiyUUID.generateUserID());
+        user.setEmailAddress(emailAddress);
+        user.setUserName(name);
+        user.setUserPassword(password);
+        docUserMapper.addDocUser(user);
+        return 0;
     }
 }
