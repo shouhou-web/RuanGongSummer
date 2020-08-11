61a9581f6a7ad288088827f4954dd61fad26e252
ShouHou <18373337@buaa.edu.cn>
2020-08-11 13:22:46 +0800

    ckeditor5初始化

--- vuec/src/views/Doc/Doc.vue
+++ vuec/src/views/Doc/Doc.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="doc">
+    <div class="editor">
+      <in-editor></in-editor>
+    </div>
   </div>
 </template>
 
 <script>
+import inEditor from "./childCpn/inEditor";
 export default {
   name: "Doc",
   components: {
-    
+    inEditor
   }
 };
 </script>
 
 <style scoped>
-@import "assets/css/base.css";
+#doc {
+  background-color: orange;
+}
 
+.editor {
+  margin: 50px auto 0;
+  width: 80vw;
+}
+
+#doc p::before {
+  content: "123";
+  position: absolute;
+  left: -9px;
+}
 </style>
