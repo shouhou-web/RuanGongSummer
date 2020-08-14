b4b686972616e8ef98823645eefad3bf0b7e1f92
Tonylyc <2290549567@qq.com>
2020-08-14 21:25:05 +0800

    删除收藏个人文档功能初步完成

--- vuec/src/views/Home/WorkSpace/MyCollection/MyCollection.vue
+++ vuec/src/views/Home/WorkSpace/MyCollection/MyCollection.vue
@@ -1,20 +1,91 @@
 <template>
-  <div class="docs">
-    <l-card class="doc" title="Test">
-      <div slot="hide-content" class="hide-nav">
-        <my-button type="text" class="nav-btn">打开</my-button>
-        <my-button type="text" class="nav-btn">收藏</my-button>
-        <my-button type="text" class="nav-btn">重命名</my-button>
-        <my-button type="text" class="nav-btn">分享</my-button>
-        <my-button type="text-danger" class="nav-btn">删除</my-button>
+  <div>
+    <div v-if="!noneShow" class="docs">
+      <div v-for="doc in myCollection" :key="doc.docID" class="doc">
+        <l-card :ID="doc.docID" :title="doc.docTitle">
+          <div slot="hide-content" class="hide-nav">
+            <my-button type="text" class="nav-btn">移出收藏</my-button>
+            <my-button type="text" class="nav-btn">重命名</my-button>
+            <my-button type="text" class="nav-btn">分享</my-button>
+            <my-button
+              type="text-danger"
+              class="nav-btn"
+              @click="deleteNotice(doc.docID)"
+              >删除</my-button
+            >
+          </div>
+        </l-card>
       </div>
-    </l-card>
+    </div>
+    <l-show-none v-else></l-show-none>
+    <m-hover
+      :onShow="docDeleteHoverOn"
+      title="删除文档"
+      assureBtn="确认"
+      cancelBtn="手滑了"
+      @cancel="cancelDelete"
+      @submit="deleteDoc"
+    >
+      <div class="hover-text">
+        删除文档后可以在回收站中还原文档，确认要删除该文档吗？
+      </div>
+    </m-hover>
   </div>
 </template>
 
 <script>
+import { getFavoriteDocs } from "network/workspace.js";
+import { deleteDoc } from "network/workspace.js";
+
 export default {
-  name: "MyCollection"
+  name: "MyCollection",
+  data() {
+    return {
+      user: "",
+      myCollection: "",
+      noneShow: false,
+      docDeleteHoverOn: false,
+      docToDeleteID: ""
+    };
+  },
+  methods: {
+    deleteNotice(docID) {
+      this.docDeleteHoverOn = true;
+      this.docToDeleteID = docID;
+    },
+    cancelDelete() {
+      this.docDeleteHoverOn = false;
+    },
+    deleteDoc() {
+      deleteDoc(this.user.userID, this.docToDeleteID).then(res => {
+        if (res === 1) {
+          this.$message.error("删除文档失败，请检查网络或联系管理员");
+        } else {
+          this.docDeleteHoverOn = false;
+          this.$message({
+            message: "删除文档成功",
+            type: "success"
+          });
+          getFavoriteDocs(this.user.userID).then(res => {
+            this.myCollection = res;
+            if (res.length === 0) this.noneShow = true;
+          });
+        }
+      });
+    }
+  },
+  created() {
+    this.user = this.$store.state.user;
+
+    if (!this.user.userID) {
+      this.$message.error("请先登录");
+      return;
+    }
+    getFavoriteDocs(this.user.userID).then(res => {
+      this.myCollection = res;
+      if (res.length === 0) this.noneShow = true;
+    });
+  }
 };
 </script>
 
