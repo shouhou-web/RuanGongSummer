45ccc01d2f4e928137f9ab66df15f639d4b372a3
ShouHou <18373337@buaa.edu.cn>
2020-08-17 23:00:36 +0800

    plugin初始化

--- vuec/src/views/Doc/childCpn/in-editor/core/ckeditor.js
+++ vuec/src/views/Doc/childCpn/in-editor/core/ckeditor.js
@@ -1,46 +1,48 @@
-import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor'
+import ClassicEditorBase from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
 
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials'
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat'
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice'
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
+import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
+import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
 
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph'
-import Heading from '@ckeditor/ckeditor5-heading/src/heading'
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
+import Heading from "@ckeditor/ckeditor5-heading/src/heading";
 
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold'
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic'
-import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline'
-import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough'
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
+import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline";
+import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough";
 
-import Link from '@ckeditor/ckeditor5-link/src/link'
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote'
-import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline'
+import Link from "@ckeditor/ckeditor5-link/src/link";
+import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
+import HorizontalLine from "@ckeditor/ckeditor5-horizontal-line/src/horizontalline";
 
-import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor'
-import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor'
+import FontColor from "@ckeditor/ckeditor5-font/src/fontcolor";
+import FontBackgroundColor from "@ckeditor/ckeditor5-font/src/fontbackgroundcolor";
 
-import Code from '@ckeditor/ckeditor5-basic-styles/src/code'
-import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock'
+import Code from "@ckeditor/ckeditor5-basic-styles/src/code";
+import CodeBlock from "@ckeditor/ckeditor5-code-block/src/codeblock";
 
-import List from '@ckeditor/ckeditor5-list/src/list'
-import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment'
+import List from "@ckeditor/ckeditor5-list/src/list";
+import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment";
 
-import Image from '@ckeditor/ckeditor5-image/src/image'
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption'
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle'
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar'
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload'
-import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize'
+import Image from "@ckeditor/ckeditor5-image/src/image";
+import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption";
+import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle";
+import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
+import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload";
+import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize";
 
-import Table from '@ckeditor/ckeditor5-table/src/table'
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar'
+import Table from "@ckeditor/ckeditor5-table/src/table";
+import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
 
-import Indent from '@ckeditor/ckeditor5-indent/src/indent'
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed'
+import Indent from "@ckeditor/ckeditor5-indent/src/indent";
+import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed";
 
+import TrackChanges from "@ckeditor/ckeditor5-track-changes/src/trackchanges";
 export default class ClassicEditor extends ClassicEditorBase {}
 
 ClassicEditor.builtinPlugins = [
+  TrackChanges,
   Essentials,
   Autoformat,
   PasteFromOffice,
@@ -78,35 +80,36 @@ ClassicEditor.builtinPlugins = [
 
   Indent,
   MediaEmbed
-]
+];
 
 ClassicEditor.defaultConfig = {
   toolbar: {
     items: [
-      'heading',
-      '|',
-      'bold',
-      'italic',
-      'underline',
-      'strikethrough',
-      '|',
-      'link',
-      'blockQuote',
-      'horizontalLine',
-      '|',
-      'fontcolor',
-      'fontbackgroundcolor',
-      '|',
-      'code',
-      'codeblock',
-      '|',
-      'bulletedList',
-      'numberedList',
-      'alignment',
-      '|',
-      'imageUpload',
-      'insertTable'
-      // '|',
+      "heading",
+      "|",
+      "bold",
+      "italic",
+      "underline",
+      "strikethrough",
+      "|",
+      "link",
+      "blockQuote",
+      "horizontalLine",
+      "|",
+      "fontcolor",
+      "fontbackgroundcolor",
+      "|",
+      "code",
+      "codeblock",
+      "|",
+      "bulletedList",
+      "numberedList",
+      "alignment",
+      "|",
+      "imageUpload",
+      "insertTable",
+      "|",
+      "TrackChanges"
       // 'indent',
       // 'outdent',
       // 'mediaEmbed'
@@ -114,23 +117,54 @@ ClassicEditor.defaultConfig = {
   },
   heading: {
     options: [
-      {model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph'},
-      {model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1'},
-      {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'},
-      {model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3'},
-      {model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4'},
+      { model: "paragraph", title: "Paragraph", class: "ck-heading_paragraph" },
+      {
+        model: "heading1",
+        view: "h1",
+        title: "Heading 1",
+        class: "ck-heading_heading1"
+      },
+      {
+        model: "heading2",
+        view: "h2",
+        title: "Heading 2",
+        class: "ck-heading_heading2"
+      },
+      {
+        model: "heading3",
+        view: "h3",
+        title: "Heading 3",
+        class: "ck-heading_heading3"
+      },
+      {
+        model: "heading4",
+        view: "h4",
+        title: "Heading 4",
+        class: "ck-heading_heading4"
+      }
     ]
   },
   image: {
-    toolbar: ['imageTextAlternative', '|', 'imageStyle:alignLeft', 'imageStyle:full', 'imageStyle:alignRight'],
-    styles: ['full', 'alignLeft', 'alignRight']
+    toolbar: [
+      "imageTextAlternative",
+      "|",
+      "imageStyle:alignLeft",
+      "imageStyle:full",
+      "imageStyle:alignRight"
+    ],
+    styles: ["full", "alignLeft", "alignRight"]
   },
   table: {
-    contentToolbar: [
-      'tableColumn',
-      'tableRow',
-      'mergeTableCells'
-    ]
+    contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"]
   },
-  language: 'en'
-}
+  licenseKey: "BTwoN2FJuJIiGvx7Ro5jkvqKKgoTNDnl1CVyzZd8RxlOrhwXD2U5FW4=",
+  TrackChanges: {
+    sidebar: {
+      container: document.querySelector("#sidebar")
+    },
+    toolbar: {
+      items: ["bold", "italic", "|", "trackChanges"]
+    }
+  },
+  language: "en"
+};
--- vuec/src/views/Doc/childCpn/in-editor/index.vue
+++ vuec/src/views/Doc/childCpn/in-editor/index.vue
@@ -1,13 +1,15 @@
 <template>
-  <ckeditor
-    class="ck-content"
-    :editor="editor"
-    v-model="editorData"
-    :disabled="editDisabled"
-    @input="onInput"
-    @focus="onFocus"
-    @blur="onBlur"
-  ></ckeditor>
+  <div>
+    <ckeditor
+      class="ck-content"
+      :editor="editor"
+      v-model="editorData"
+      :disabled="editDisabled"
+      @input="onInput"
+      @focus="onFocus"
+      @blur="onBlur"
+    ></ckeditor>
+  </div>
 </template>
 
 <script>
@@ -70,6 +72,7 @@ export default {
       this.onBlur();
     }
     this.editorData = this.doc.docContent;
+    this._initEdit();
   },
   mounted() {
     window.addEventListener("beforeunload", function (e) {});
@@ -87,7 +90,7 @@ export default {
     onInput() {
       // console.log(this.value);
       editDoc(this.doc.docID, this.editorData).then((res) => {
-        console.log("编辑状态", res);
+        // console.log("编辑状态", res);
         if (res == 1) {
           this.$notify.error({
             title: "网络错误",
@@ -98,9 +101,9 @@ export default {
     },
     // 聚焦事件
     onFocus() {
-      console.log("onFocus");
+      // console.log("onFocus");
       tryEditDoc(this.doc.docID).then((res) => {
-        console.log("聚焦事件返回:", res);
+        // console.log("聚焦事件返回:", res);
         if (res != 0) {
           this.editState = true;
           this.$message({
@@ -112,10 +115,10 @@ export default {
     },
     // 失焦事件
     onBlur() {
-      console.log("onBlur");
+      // console.log("onBlur");
       completeEditDoc(this.$store.state.user.userID, this.doc.docID).then(
         (res) => {
-          console.log("失焦事件返回:", res);
+          // console.log("失焦事件返回:", res);
           if (res == 0)
             this.$notify({
               title: "成功",
@@ -129,6 +132,118 @@ export default {
             });
         }
       );
+    },
+    _initEdit() {
+      const appData = {
+        // Users data.
+        users: [
+          {
+            id: "user-1",
+            name: "Joe Doe",
+            // Note that the avatar is optional.
+            avatar: "https://randomuser.me/api/portraits/thumb/men/26.jpg",
+          },
+          {
+            id: "user-2",
+            name: "Ella Harper",
+            avatar: "https://randomuser.me/api/portraits/thumb/women/65.jpg",
+          },
+        ],
+
+        // The ID of the current user.
+        userId: "user-1",
+
+        // Suggestions data.
+        suggestions: [
+          {
+            id: "suggestion-1",
+            type: "insertion",
+            authorId: "user-2",
+            createdAt: new Date(2019, 1, 13, 11, 20, 48),
+          },
+          {
+            id: "suggestion-2",
+            type: "deletion",
+            authorId: "user-1",
+            createdAt: new Date(2019, 1, 14, 12, 7, 20),
+          },
+          {
+            id: "suggestion-3",
+            type: "formatInline:886cqig6g8rf",
+            authorId: "user-1",
+            createdAt: new Date(2019, 2, 8, 10, 2, 7),
+            data: {
+              commandName: "bold",
+              commandParams: [{ forceValue: true }],
+            },
+          },
+        ],
+
+        // Editor initial data.
+        initialData: `<h2>
+          Bilingual Personality Disorder
+      </h2>
+      <p>
+          This may be the first time you hear about this
+          <suggestion id="suggestion-1:user-2" suggestion-type="insertion" type="start"></suggestion>
+          made-up<suggestion id="suggestion-1:user-2" suggestion-type="insertion" type="end"></suggestion>
+          disorder but it actually isn’t so far from the truth.
+          As recent studies show, the language you speak has more effects on you than you realize.
+          According to the studies, the language a person speaks affects their cognition,
+          <suggestion id="suggestion-2:user-1" suggestion-type="deletion" type="start"></suggestion>
+          feelings, <suggestion id="suggestion-2:user-1" suggestion-type="deletion" type="end"></suggestion>
+          behavior, emotions and hence <strong>their personality</strong>.
+      </p>
+      <p>
+          This shouldn’t come as a surprise
+          <a href="https://en.wikipedia.org/wiki/Lateralization_of_brain_function">since we already know</a>
+          that different regions of the brain become more active depending on the activity.
+          The structure, information and especially
+          <suggestion id="suggestion-3:user-1" suggestion-type="formatInline:886cqig6g8rf" type="start"></suggestion>
+          the culture of languages<suggestion id="suggestion-3:user-1" suggestion-type="formatInline:886cqig6g8rf" type="end"></suggestion>
+          varies substantially
+          and the language a person speaks is an essential element of daily life.
+      </p>`,
+      };
+
+      class TrackChangesIntegration {
+        constructor(editor) {
+          this.editor = editor;
+        }
+
+        init() {
+          const usersPlugin = this.editor.plugins.get("Users");
+          const trackChangesPlugin = this.editor.plugins.get("TrackChanges");
+
+          // Load the users data.
+          for (const user of appData.users) {
+            usersPlugin.addUser(user);
+          }
+
+          // Set the current user.
+          usersPlugin.defineMe(appData.userId);
+
+          // Load the suggestions data.
+          for (const suggestion of appData.suggestions) {
+            trackChangesPlugin.addSuggestion(suggestion);
+          }
+
+          // In order to load comments added to suggestions, you
+          // should also configure the comments integration.
+        }
+      }
+
+      ClassicEditor.create(document.querySelector("#editor"), {
+        initialData: appData.initialData,
+        extraPlugins: [TrackChangesIntegration],
+        licenseKey: "BTwoN2FJuJIiGvx7Ro5jkvqKKgoTNDnl1CVyzZd8RxlOrhwXD2U5FW4=",
+        sidebar: {
+          container: document.querySelector("#sidebar"),
+        },
+        toolbar: {
+          items: ["bold", "italic", "|", "trackChanges"],
+        },
+      }).catch((error) => console.error(error));
     },
   },
 };
